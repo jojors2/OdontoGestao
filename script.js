5e355b8f492d0c2de1cc46be8ecc70f467fc605e
jojors2 <joelleramos02@gmail.com>
2026-08-24 19:20:27 -0300

    Dashboard

--- script.js
+++ script.js
@@ -91,7 +91,6 @@ loginForm.addEventListener("submit", function(event) {
     });
 
     if (formularioValido) {
-        alert("Login realizado com sucesso!");
         window.location.href = "dashboard.html";
     }
 });
